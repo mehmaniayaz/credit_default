b08c4a5631c6ec00eacadc3df2bdfd1ad5a9bfb2
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-10-22 16:16:25 -0500

    passing test added

--- src/setup.py
+++ src/setup.py
@@ -7,8 +7,8 @@ import random
 def generate_cat_num_dataframe(n_samples=100, n_features=10, n_cats=2):
     X, y = make_classification(n_samples=n_samples, n_features=n_features, n_redundant=2)
     df = pd.DataFrame(X, columns=['feature_' + str(x) for x in range(np.shape(X)[1])])
-    df['target'] = y
     cat_feature_list = random.sample(list(df.columns), n_cats)
+    df['target'] = y
 
     bins_list = [3, 4, 5]
     dict_col = {}
--- test/test_setup.py
+++ test/test_setup.py
@@ -4,7 +4,8 @@ from src.setup import *
 
 class TestSetUp(unittest.TestCase):
     def test_generate_cat_num_dataframe(self):
-        df = generate_cat_num_classification_dataframe(n_samples=100, n_features=10, n_cats=2)
-        self.assertEqual(len(df._get_numeric_data().columns), 10)
+        df = generate_cat_num_dataframe(n_samples=100, n_features=10, n_cats=2)
+        #generated dataframe will in fact have 11 features with the added binary target column (numerical)
+        self.assertEqual(len(df._get_numeric_data().columns), 10+1-2)
         self.assertEqual(len(df), 100)
-        self.assertEqual(len(df.columns), 10)
+        self.assertEqual(len(df.columns), 11)
