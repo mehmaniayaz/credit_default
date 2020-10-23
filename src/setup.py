94fe3d264a9ab8a5b60a964447d261a05d9fec48
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-10-23 10:57:55 -0500

    passing test for encoding list of strings added

--- src/setup.py
+++ src/setup.py
@@ -23,6 +23,24 @@ def generate_cat_num_dataframe(n_samples=100, n_features=10, n_cats=2):
     for feature in cat_feature_list:
         dict_col[feature] = 'cat_' + feature
         bin_number = random.sample(bins_list, 1)[0]
-        df[feature] = pd.cut(df[feature], bins=bin_number, labels=['lbl_'+str(x) for x in range(bin_number)])
+        df[feature] = pd.cut(df[feature], bins=bin_number, labels=['lbl_' + str(x) for x in range(bin_number)])
     df.rename(columns=dict_col, inplace=True)
     return df
+
+
+def encode_string_list(x):
+    """
+    Summary: take a list of strings x and encode its values to a list of integers.
+    :param x: list of strings
+    :return y: list of integers that are codes for x. x is sorted before being encoded.
+
+    Example:
+    x = ['cat', 'dog', 'book', 'pencil', 'dog', 'book']
+    y = [1, 2, 0, 3, 2, 0]
+    """
+    x_sorted = np.sort(x)
+    keys = np.unique(x_sorted)
+    values = list(range(len(keys)))
+    dict_encode = dict(zip(keys, values))
+    y = [dict_encode[x] for x in x]
+    return y
