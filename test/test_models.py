20b4ba6a55d0657851e3c4660d99d09682f53fc8
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-11-06 13:21:34 -0600

    fail test written

--- test/test_models.py
+++ test/test_models.py
@@ -5,5 +5,9 @@ from src.models import *
 
 class TestSetUp(unittest.TestCase):
     def test_alphabetize_ordinals(self):
-        alpha_arr = alphabetize_ordinals(df,list_ordinals)
-        self.assertTrue(alpha_arr,test_arr)
+        df = pd.DataFrame(columns={'ordinal_feature', 'numerical_feature'})
+        df['numerical_feature'] = np.random.choice(range(10), size=(5))
+        df['ordinal_feature'] = ['carrot', 'cake', 'apple', 'Pony', 'BanAna']
+        encoded_arr_true = [3, 2, 0, 5, 1]
+        encoded_arr_predicted = np.array(alphabetize_ordinals(df))
+        self.assertTrue(encoded_arr_true , encoded_arr_predicted)
