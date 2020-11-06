80a5b4067e8236ad6167cc5003e55beb760cdb1e
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-11-06 13:32:35 -0600

    some function writing for alphabetizing ordinal features of a dataframe

--- src/models.py
+++ src/models.py
@@ -3,13 +3,23 @@ import numpy as np
 from sklearn.datasets import make_classification
 import random
 
-def alphabetize_ordinals(df):
+
+def alphabetize_ordinals(df,list_ordinals):
     """
     Rename ordinal features of a dataframe based on their order in list_ordinals
     Example: dict_education = {"grad":"A_grad","undergrad":"B_undergrad","high-school":"C_high-school","others":"D_others"}
 
-
+    list_ordinals: list of ordinal values that require sorting
     :param df: single column dataframe with the ordinal feature selected
     :return: a column dataframe with the feature values encoded
     """
 
+    if len(df.columns) != 1:
+        raise ValueError("dataframe needs to have a single column")
+
+    dict_rename = dict()
+    for i in range(len(list_ordinals)):
+        dict_rename[list_ordinals[i]] = str(i) + '__' + list_ordinals[i]
+
+    df = df.replace({df.columns[0]: dict_rename})
+    return df
--- test/test_models.py
+++ test/test_models.py
@@ -7,7 +7,8 @@ class TestSetUp(unittest.TestCase):
     def test_alphabetize_ordinals(self):
         df = pd.DataFrame(columns={'ordinal_feature', 'numerical_feature'})
         df['numerical_feature'] = np.random.choice(range(10), size=(5))
-        df['ordinal_feature'] = ['carrot', 'cake', 'apple', 'Pony', 'BanAna']
-        encoded_arr_true = [3, 2, 0, 5, 1]
-        encoded_arr_predicted = np.array(alphabetize_ordinals(df))
+        df['ordinal_feature'] = ['carrot', 'cake', 'apple', 'pony', 'banana']
+        encoded_arr_true = [1, 0, 4, 3, 2]
+        ordinal_list = ['cake', 'carrot', 'banana', 'pony', 'apple']
+        encoded_arr_predicted = np.array(alphabetize_ordinals(df, ordinal_list))
         self.assertTrue((encoded_arr_true == encoded_arr_predicted).all())
